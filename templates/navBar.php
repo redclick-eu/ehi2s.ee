390c38d025017da4168582893a1477d9a4622d7b
Daniel Dubchenko <daniel217032001@gmail.com>
2018-06-11 14:33:00 +0300

    feature($navbar $header)
    Add submenu to full window menu
    Fixed submenu in common menu

--- templates/navBar.php
+++ templates/navBar.php
@@ -1,18 +1,18 @@
-<div class="container navBar ">
+    <div class="container navBar ">
     <div class="row">
         <nav class="col-7">
             <ul class="menu">
-                <li class="menu-item"><a href="" class="menu-url">главная</a></li>
-                <li class="menu-item"><a href="" class="menu-url">услуги</a>
+                <li class="menu-item"><a href="<?= home_url('/'); ?>" class="menu-url">главная</a></li>
+                <li class="menu-item"><a href="<?= home_url('/'); ?>" class="menu-url">услуги</a>
                     <ul class="menu menu_sub">
-                        <li class="menu_sub-item"><a href="">бетонные полы</a></li>
-                        <li class="menu_sub-item"><a href="">фундамент</a></li>
-                        <li class="menu_sub-item"><a href="">общестроительные работы</a></li>
+                        <li class="menu_sub-item"><a href="<?= home_url('/'); ?>">бетонные полы</a></li>
+                        <li class="menu_sub-item"><a href="<?= home_url('/'); ?>">фундамент</a></li>
+                        <li class="menu_sub-item"><a href="<?= home_url('/'); ?>">общестроительные работы</a></li>
                     </ul>
                 </li>
-                <li class="menu-item"><a href="" class="menu-url">проекты</a></li>
-                <li class="menu-item"><a href="" class="menu-url">о нас</a></li>
-                <li class="menu-item"><a href="" class="menu-url">контакты</a></li>
+                <li class="menu-item"><a href="<?= home_url('/'); ?>" class="menu-url">проекты</a></li>
+                <li class="menu-item"><a href="<?= home_url('/'); ?>" class="menu-url">о нас</a></li>
+                <li class="menu-item"><a href="<?= home_url('/'); ?>" class="menu-url">контакты</a></li>
             </ul>
         </nav>
         <div class="col-3 search">
