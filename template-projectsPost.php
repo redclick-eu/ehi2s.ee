42aaff186ce15b0fbd377275585687acebffdb41
Daniel Dubchenko <daniel217032001@gmail.com>
2018-06-18 20:09:11 +0300

    fix(template-projectPost.php) change acf

--- template-projectsPost.php
+++ template-projectsPost.php
@@ -9,13 +9,13 @@
 <div class=" container projectsPost">
     <?php get_template_part('templates/pageHeader'); ?>
     <div class="row">
-        <?php if (have_rows('about')): while (have_rows('about')) : the_row();
-            $photo = get_sub_field('about_photo');
+        <?php if (have_rows('project')): while (have_rows('project')) : the_row();
+            $photo = get_sub_field('project_photo');
             ?>
             <div class="col-12 col-md-6 projectsPost-item">
                 <img class="projectsPost-img" src="<?= $photo['sizes']['large'] ?>" alt="<?= $photo['alt'] ?>" title="<?= $photo['title'] ?>">
-                <h3 class="projectsPost-textHeader"><?= get_sub_field('about_header') ?></h3>
-                <?= get_sub_field('about_text') ?>
+                <h3 class="projectsPost-textHeader"><?= get_sub_field('project_header') ?></h3>
+                <?= get_sub_field('project_text') ?>
             </div>
         <?php
         endwhile;endif; ?>
