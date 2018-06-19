f09fb701df1c2f31542b3edee2e42bea7df2d86d
Daniel Dubchenko <daniel217032001@gmail.com>
2018-06-19 16:00:00 +0300

    feature($services) add addition post from admin panel
    And added some style fixes

--- templates/services.php
+++ templates/services.php
@@ -1,35 +1,34 @@
 <div class=" container services">
     <h2 class="page-header">Услуги</h2>
     <div class="row">
-        <div class="col-12 col-lg-4">
-            <a href="<?= home_url('/'); ?>" class="services-item">
-                <div class="services-iconContainer">
-                    <img src="<?= get_template_directory_uri(); ?>/dist/images/bg/serv_p_1.png" alt="" class="services-photo">
-                    <div class="services-icon services-icon_1"></div>
-                </div>
-                <h3 class="services-header">Бетонные полы</h3>
-                <p class="services-text">Fat new smallness few supposing suspicion two. Course sir people worthy horses add entire.</p>
-            </a>
-        </div>
-        <div class="col-12 col-lg-4">
-            <a href="<?= home_url('/'); ?>" class="services-item">
-                <div class="services-iconContainer">
-                    <img src="<?= get_template_directory_uri(); ?>/dist/images/bg/serv_p_2.png" alt="" class="services-photo">
-                    <div class="services-icon services-icon_2"></div>
-                </div>
-                <h3 class="services-header">Фундамент</h3>
-                <p class="services-text">Fat new smallness few supposing suspicion two. Course sir people worthy horses add entire.</p>
-            </a>
-        </div>
-        <div class="col-12 col-lg-4">
-            <a href="<?= home_url('/'); ?>" class="services-item">
-                <div class="services-iconContainer">
-                    <img src="<?= get_template_directory_uri(); ?>/dist/images/bg/serv_p_3.png" alt="" class="services-photo">
-                    <div class="services-icon services-icon_3"></div>
-                </div>
-                <h3 class="services-header">Общестроительные <br> работы</h3>
-                <p class="services-text">Fat new smallness few supposing suspicion two. Course sir people worthy horses add entire.</p>
-            </a>
-        </div>
+        <?php
+        $args = [
+            'category_name' => 'services',
+        ];
+        $list = new WP_Query($args);
+        $projects;
+        if ($list->have_posts()) : while ($list->have_posts()) : $list->the_post();
+            $projects[get_field('project_place')] =   '
+            <div class="col-12 col-lg-4">
+                <a href="'. get_permalink()  .'" class="services-item">
+                    <div class="services-iconContainer">
+                        <img src="'. get_field('project_logo')['url'] .'"
+                             alt="'. get_field('project_logo')['alt'] .'"
+                             title="'. get_field('project_logo')['title'] .'" class="services-photo"">
+                        <div class="services-icon services-icon_'. get_field('project_icon') .'"></div>
+                    </div>
+                    <h3 class="services-header">'. get_the_title() .'</h3>
+                    <p class="services-text">'.  get_field('project_about') .'</p>
+                </a>
+            </div>
+        ';
+        endwhile;
+        endif;
+        wp_reset_postdata();
+        echo $projects['left'];
+        echo $projects['mid'];
+        echo $projects['right'];
+        ?>
+
     </div>
 </div>
