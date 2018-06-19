749c1bfd9541d848bd21beb244865c661630dd46
Daniel Dubchenko <daniel217032001@gmail.com>
2018-06-19 19:05:57 +0300

    fix(lang.php) add new language switcher

--- templates/lang.php
+++ templates/lang.php
@@ -1,8 +1,8 @@
-<?php if(ICL_LANGUAGE_CODE === 'ru'):
-    $wpml_permalink = apply_filters( 'wpml_permalink', get_the_permalink() , 'est' );?>
+<?php
+$l = icl_get_languages('skip_missing=0&orderby=code');
+if ($l['ru']['active']) {
+    ?><a href="<?= $l['et']['url'] ?>" class="lang-item lang-item_est lang-item_active"> est </a> <span class="lang-item lang-item_slash">/</span> <span class="lang-item lang-item_ru"> ru </span><?php
+} else {
+    ?><span class="lang-item lang-item_est "> est </span> <span class="lang-item lang-item_slash">/</span> <a href="<?= $l['ru']['url'] ?>" class="lang-item lang-item_ru lang-item_active"> ru </a><?php
+}
 
-<a href="<?= $wpml_permalink?>" class="lang-item lang-item_est lang-item_active"> est </a> <span class="lang-item lang-item_slash">/</span> <span class="lang-item lang-item_ru"> ru </span>
-<?php else:
-    $wpml_permalink = apply_filters( 'wpml_permalink', get_the_permalink() , 'ru' );?>
-<span class="lang-item lang-item_est "> est </span> <span class="lang-item lang-item_slash">/</span> <a href="<?= $wpml_permalink?>" class="lang-item lang-item_ru lang-item_active"> ru </a>
-<?php endif; ?>
