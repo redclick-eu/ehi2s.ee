dd94bbd7d55baf73e4b913d8c3f1b4682dfa54c9
Daniel Dubchenko <daniel217032001@gmail.com>
2018-06-21 13:45:24 +0300

    feature($dictionary) update translate function

--- lib/dictionary.php
+++ lib/dictionary.php
@@ -2,58 +2,29 @@
 
 namespace Roots\Sage\Dictionary;
 
-function tr($num, $echo)
+function tr($str, $echo)
 {
     $dictionary = array(
-        '0' => array(
-            'ru' => 'ru',
-            'et' => 'est'
-        ),
-        '1' => array(
-            'ru' => ' Вы используете <strong>устаревший</strong> браузер. Пожалуйста <a href="http://browser.yandex.ru/">обновите свой браузер</a> для корректного отображения страниц сайта.',
-            'et' => 'Kasutate  <strong>vananenud </strong> brauserit. Palun <a href="http://browser.yandex.ru/"> värskendage oma brauserit</a> et kuvada saidi õiged lehed.',
-        ),
-        '2' => array(
-            'ru' => 'Главная',
-            'et' => 'Kodulehekülg',
-        ),
-        '3' => array(
-            'ru' => 'Результаты поиска по запросу',
-            'et' => 'Otsingutulemused',
-        ),
-        '4' => array(
-            'ru' => 'Записи с тегом',
-            'et' => 'Märgistatud',
-        ),
-        '5' => array(
-            'ru' => 'Статьи автора',
-            'et' => 'Autor artiklid',
-        ),
-        '6' => array(
-            'ru' => 'Ошибка 404',
-            'et' => 'viga 404',
-        ),
-        '7' => array(
-            'ru' => 'Страница комментариев',
-            'et' => 'Kommentaari lehekülg',
-        ),
-        '8' => array(
-            'ru' => 'Главная',
-            'et' => 'Kodulehekülg',
-        ),
-        '9' => array(
-            'ru' => 'Главная',
-            'et' => 'Kodulehekülg',
-        ),
-        '10' => array(
-            'ru' => 'Главная',
-            'et' => 'Kodulehekülg',
-        ),
+        'ru' => 'est',
+        'Вы используете <strong>устаревший</strong> браузер. Пожалуйста <a href="http://browser.yandex.ru/">обновите свой браузер</a> для корректного отображения страниц сайта.' => 'Kasutate  <strong>vananenud </strong> brauserit. Palun <a href="http://browser.yandex.ru/"> värskendage oma brauserit</a> et kuvada saidi õiged lehed.',
+        'Главная' =>  'Kodulehekülg',
+        'Результаты поиска по запросу' => 'Otsingutulemused',
+        'Записи с тегом' => 'Märgistatud',
+        'Статьи автора' => 'Autor artiklid',
+        'Ошибка 404' => 'viga 404',
+        'Страница комментариев' => 'Kommentaari lehekülg',
     );
-    if ($echo) {
-        echo $dictionary[$num][ICL_LANGUAGE_CODE];
+    if(ICL_LANGUAGE_CODE === 'et') {
+        if(isset($dictionary[$str])) {
+            $str = $dictionary[$str];
+        } else {
+            $str = 'Not such string in array';
+        }
     }
-    return $dictionary[$num][ICL_LANGUAGE_CODE];
+    if ($echo) {
+        echo $str;
+    }
+    return $str;
 }
 
 add_filter('tr', __NAMESPACE__ . '\\tr',10,2);
