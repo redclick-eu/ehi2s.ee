f64f3e6995c8e375d6fa9f774fd7c5de45a56971
Daniel Dubchenko <daniel217032001@gmail.com>
2018-06-28 22:51:05 +0300

    update($images) add favicon, remove unused photos

--- templates/head.php
+++ templates/head.php
@@ -2,5 +2,7 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" href="<?= get_template_directory_uri(); ?>/dist/images/favicon.ico" type="image/x-icon"><?php //подключение favicon?>
+  <link rel="shortcut icon" href="<?= get_template_directory_uri(); ?>/dist/images/favicon.ico" type="image/x-icon" />
   <?php wp_head(); ?>
 </head>
