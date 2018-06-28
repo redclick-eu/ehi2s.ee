d03928beaf0f046d7f6327162342fdaa0f646cc9
vb@vb-pc <vb@vb-pc>
2018-06-28 23:32:53 +0300

    lang item changes

--- templates/lang.php
+++ templates/lang.php
@@ -1,8 +1,8 @@
 <?php
 $l = icl_get_languages('skip_missing=0&orderby=code');
 if ($l['ru']['active']) {
-    ?><a href="<?= $l['et']['url'] ?>" class="lang-item lang-item_est lang-item_active"> est </a> <span class="lang-item lang-item_slash">/</span> <span class="lang-item lang-item_ru"> ru </span><?php
+    ?><a href="<?= $l['et']['url'] ?>" class="lang-item lang-item_est lang-item_active"> eesti </a> <span class="lang-item lang-item_slash">/</span> <span class="lang-item lang-item_ru"> русский </span><?php
 } else {
-    ?><span class="lang-item lang-item_est "> est </span> <span class="lang-item lang-item_slash">/</span> <a href="<?= $l['ru']['url'] ?>" class="lang-item lang-item_ru lang-item_active"> ru </a><?php
+    ?><span class="lang-item lang-item_est "> eesti </span> <span class="lang-item lang-item_slash">/</span> <a href="<?= $l['ru']['url'] ?>" class="lang-item lang-item_ru lang-item_active"> русский </a><?php
 }
 
