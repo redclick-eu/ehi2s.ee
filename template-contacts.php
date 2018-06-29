298bb5d62af3e40b244f1b35494c3d7c8e7ffc00
Daniel Dubchenko <daniel217032001@gmail.com>
2018-06-29 21:12:14 +0300

    feature($site) add new phrases to rc-translate

--- template-contacts.php
+++ template-contacts.php
@@ -17,7 +17,7 @@
                 <p class="contacts-text"><span class="font-weight-bold"><?= get_field('i_name'); ?></span></p>
                 <p class="contacts-text"><span class="font-weight-bold"><?php rc_t('Адрес', 1) ?>: </span><?= get_field('i_adress'); ?></p>
                 <p class="contacts-text"><span class="font-weight-bold"><?php rc_t('Телефон', 1) ?>: </span><?= get_field('i_phone'); ?></p>
-                <p class="contacts-text"><span class="font-weight-bold">E-post: </span><?= get_field('i_mail'); ?></p>
+                <p class="contacts-text"><span class="font-weight-bold"><?php rc_t('E-mail', 1) ?>: </span><?= get_field('i_mail'); ?></p>
                 <p class="contacts-text"><span class="font-weight-bold">Reg kood: </span><?= get_field('i_rk'); ?></p>
                 <p class="contacts-text"><span class="font-weight-bold">KMKR: </span><?= get_field('i_kmkr'); ?></p>
                 <p class="contacts-text"><span class="font-weight-bold">SWEDBANK: </span><?= get_field('i_swedbank'); ?></p>
