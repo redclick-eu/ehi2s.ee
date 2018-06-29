69eeb4965e9827af8de767059e29f9bfb213692a
Daniel Dubchenko <daniel217032001@gmail.com>
2018-06-29 16:05:18 +0300

    update

--- templates/header.php
+++ templates/header.php
@@ -13,7 +13,7 @@
                             'meta_key' => '_wp_page_template',
                             'meta_value' => 'template-contacts.php'
                         ))[0] -> guid;
-                        ?>
+                        $contactsUrl = apply_filters( 'wpml_permalink', $contactsUrl , ICL_LANGUAGE_CODE );?>
                         <a href="tel:<?= get_field('i_phone_url');?>" class="col-6 col-xl-3 info-item info-item_phone">
                             <span class="info-text"><?= get_field('i_phone');?></span>
                         </a>
@@ -41,7 +41,7 @@
                 <a href="<?= home_url('/'); ?>"><img src="<?= get_template_directory_uri(); ?>/dist/images/logo.png" alt="logo" class="header-logo header-logo_smallMenu"> </a>
             </div>
             <div class="col-2 p-5 d-flex align-items-center">
-                <a class="header-btn header-btn_phone" href="#"></a>
+                <a class="header-btn header-btn_phone" href="<?= get_field('i_phone_url');?>"></a>
             </div>
             <div class="col-2 d-flex p-5 align-items-center">
                 <div class="header-btn header-btn_menu" data-toggle="modal" data-target="#navbar_top"></div>
--- templates/navBar-top.php
+++ templates/navBar-top.php
@@ -1,19 +1,20 @@
 <div class="d-flex d-lg-none header_fullScreen collapse flex-column" id="navbar_top">
     <div class="container">
         <div class="row">
-            <div class="col-3 lang">
+            <div class="col-12 col-sm-5 lang">
                 <?php get_template_part('templates/lang') ?>
             </div>
-            <div class="col-9 search">
+            <div class="col-12 col-sm-7 search">
                 <?php get_template_part('templates/search'); ?>
             </div>
 
         </div>
-        <nav class="row">
+        <nav class="row d-flex justify-content-center justify-content-sm-start">
             <?php
             wp_nav_menu( array(
                 'menu_class'=>'d-flex flex-column',
                 'theme_location'=>'primary_navigation',
+                'container'       => false
             ) );
             ?>
         </nav>
