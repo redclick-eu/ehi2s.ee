93e5e9f6a70c6f37348d1f21140f959927f3f39c
Daniel Dubchenko <daniel217032001@gmail.com>
2018-07-15 19:41:09 +0300

    update gulp.js

--- templates/head.php
+++ templates/head.php
@@ -1,9 +1,15 @@
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="icon" href="<?= get_template_directory_uri(); ?>/dist/images/favicon.ico" type="image/x-icon"><?php //подключение favicon?>
-  <link rel="shortcut icon" href="<?= get_template_directory_uri(); ?>/dist/images/favicon.ico" type="image/x-icon" />
-  <link href="https://fonts.googleapis.com/css?family=Roboto:400,700&amp;subset=cyrillic" rel="stylesheet">
-  <?php wp_head(); ?>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="<?= get_template_directory_uri(); ?>/dist/images/favicon.ico" type="image/x-icon"><?php //подключение favicon?>
+    <link rel="shortcut icon" href="<?= get_template_directory_uri(); ?>/dist/images/favicon.ico" type="image/x-icon"/>
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,700&amp;subset=cyrillic" rel="stylesheet">
+    <!-- Chrome, Firefox OS and Opera -->
+    <meta name="theme-color" content="#ed8d22">
+    <!-- Windows Phone -->
+    <meta name="msapplication-navbutton-color" content="#ed8d22">
+    <!-- iOS Safari -->
+    <meta name="apple-mobile-web-app-status-bar-style" content="#ed8d22">
+    <?php wp_head(); ?>
 </head>
