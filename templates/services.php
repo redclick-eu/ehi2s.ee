930aaf61eb791cee4612a7a152d706058125341c
Daniel Dubchenko <daniel217032001@gmail.com>
2018-07-17 21:56:34 +0300

    change image resulution on template services

--- templates/services.php
+++ templates/services.php
@@ -12,7 +12,7 @@
             <div class="col-12 col-lg-4">
                 <a href="'. get_permalink()  .'" class="services-item">
                     <div class="services-iconContainer">
-                        <img src="'. get_field('project_logo')['url'] .'"
+                        <img src="'. get_field('project_logo')['sizes']['thumbnail'] .'"
                              alt="'. get_field('project_logo')['alt'] .'"
                              title="'. get_field('project_logo')['title'] .'" class="services-photo"">
                         <div class="services-icon services-icon_'. get_field('project_icon') .'"></div>
